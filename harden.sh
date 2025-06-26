f3f1ab36bf5ab6269d06ea55fb316202430f2711
arshad rahman <arshadmp004@gmail.com>
2025-06-26 15:12:09 +0530

    Update harden.sh
    script added

--- harden.sh
+++ harden.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+# update & upgrade
+apt update && apt -y upgrade
 
+# disable root SSH login
+sed -i 's/^PermitRootLogin .*/PermitRootLogin no/' /etc/ssh/sshd_config
+
+# set up UFW firewall
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw --force enable
+
+# install fail2ban
+apt -y install fail2ban
+systemctl enable fail2ban
+
+echo "Basic Server hardening complete!"
